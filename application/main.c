cbe5266c252033a2285e92fce2b36bea3ae3fdb5
james <1943357252@qq.com>
2024-07-29 11:42:10 +0800

    add lcd

---
+++ application/main.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2023-5-10      ShiHao       first version
+ */
+
+#include <rtthread.h>
+#include <rtdevice.h>
+#include <board.h>
+
+#define DBG_TAG "main"
+#define DBG_LVL DBG_LOG
+#include <rtdbg.h>
+
+#include <drv_lcd.h>
+#include <rttlogo.h>
+
+/* 配置 LED 灯引脚 */
+#define PIN_LED_B              GET_PIN(F, 11)      // PF11 :  LED_B        --> LED
+#define PIN_LED_R              GET_PIN(F, 12)      // PF12 :  LED_R        --> LED
+
+int main(void)
+{
+    // lcd_clear(WHITE);
+
+    // /* show RT-Thread logo */
+    // lcd_show_image(0, 0, 240, 69, image_rttlogo);
+
+    // /* set the background color and foreground color */
+    // lcd_set_color(WHITE, BLACK);
+
+    // /* show some string on lcd */
+    // lcd_show_string(10, 69, 16, "Hello, RT-Thread!");
+    // lcd_show_string(10, 69 + 16, 24, "RT-Thread");
+    // lcd_show_string(10, 69 + 16 + 24, 32, "RT-Thread");
+
+    // /* draw a line on lcd */
+    // lcd_draw_line(0, 69 + 16 + 24 + 32, 240, 69 + 16 + 24 + 32);
+
+    // /* draw a concentric circles */
+    // lcd_draw_point(120, 194);
+    // for (int i = 0; i < 46; i += 4)
+    // {
+    //     lcd_draw_circle(120, 194, i);
+    // }
+    return 0;
+}
+
